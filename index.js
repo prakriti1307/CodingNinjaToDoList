d90c1b05d2fe9d72ee24508865e8225c6255208f
Prakriti Kumari <prakriti_kumari@persistent.com>
2023-09-18 13:40:20 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express=require('express');
+const app=express();
+const port=8000;
+const path=require('path');
+
+// Set up view engine 
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+app.use(express.urlencoded());
+app.use(express.static('assets'));
+
+
+app.post("/tasks", (req, res) => {
+    res.send("Hello");
+ });
+
+// Port
+app.listen(port,function(err){
+    if(err){
+        console.log("Error : ",err);
+    }
+    console.log(`Server is running on port : ${port}`);
+});
